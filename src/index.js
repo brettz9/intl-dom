49f13ae1a81880a359c669ddec81b26cd37309d4
Brett Zamir <brettz9@yahoo.com>
2019-10-21 02:33:28 +0800

    INCOMPLETE: literal brackets, document feat: make localization strategy customizable (e.g., not necessarily getting on `message` property); provide two default named options

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 // Todo: Allow literal brackets (with or without substitutions
 //   of the same name present)
-// Todo: Make localization strategy customizable (e.g., not necessarily
-//   getting on `message` property); provide two named options
-// Todo: Separate out locale retrieval logic from parsing logic and export both!
 
 /**
 * @callback PromiseChainErrback
@@ -36,7 +33,7 @@
    });
  });
  */
-const promiseChainForValues = (values, errBack) => {
+export const promiseChainForValues = (values, errBack) => {
   return values.reduce(async (p, value) => {
     try {
       return await p; // We'd short-circuit here instead if we could
@@ -49,18 +46,15 @@ const promiseChainForValues = (values, errBack) => {
 };
 
 /**
-* @callback DefaultLocaleStringResolver
-* @param {string} key
-* @param {LocaleStringObject} strings
-* @returns {string} The translation
-*/
-
-/**
 * @callback SubstitutionCallback
 * @param {string} arg Accepts the second portion of the `bracketRegex` of
 *   `i18n`, i.e., the non-bracketed segments of text from the locale string
 *   following a bracketed segment.
 * @returns {string} The replacement text
+*/
+
+/**
+* @typedef {Object<string, string>} PlainLocaleStringObject
 */
 
 /**
@@ -82,7 +76,7 @@ const promiseChainForValues = (values, errBack) => {
 */
 
 /**
-* @typedef {Object<string, string|Element|SubstitutionCallback>}
+* @typedef {PlainObject<string, string|Element|SubstitutionCallback>}
 *   SubstitutionObject
 */
 
@@ -100,38 +94,40 @@ const promiseChainForValues = (values, errBack) => {
  * @param {false|SubstitutionObject} [substitutions=false]
  * @param {PlainObject} [cfg={}]
  * @param {boolean} [cfg.dom=false]
+ * @returns {string|DocumentFragment}
 */
 
 /**
  * @type {LocaleResolver}
  */
-const DefaultLocaleResolver = (locale, localesBasePth) => {
+export const DefaultLocaleResolver = (locale, localesBasePth) => {
   return `${localesBasePth.replace(/\/$/u, '')}/_locales/${locale}/messages.json`;
 };
+
+/**
+* @callback MessageStyleCallback
+* @param {LocaleStringObject|PlainLocaleStringObject|PlainObject} obj The exact
+*   format depends on the `cfg.defaults` of `i18n`
+* @param {string} key
+* @returns {false|string} If `false`, will resort to default
+*/
 
 /**
  * @param {PlainObject} [cfg]
  * @param {string[]} [cfg.locales=navigator.languages] BCP-47 language strings
  * @param {string[]} [cfg.defaultLocales=['en-US']]
- * @param {DefaultLocaleStringResolver|false|LocaleStringObject} [cfg.defaults]
- * @param {RegExp} [cfg.bracketRegex=/\{([^}]*?)(?:\|([^}]*))?\}/gu]
- * @param {boolean} [cfg.forceNodeReturn=false]
  * @param {string} [cfg.localesBasePath='.']
  * @param {LocaleResolver} [cfg.localeResolver=DefaultLocaleResolver]
- * @returns {Promise<I18NCallback>} Rejects if no suitable locale is found.
+ * @returns {Promise<LocaleStringObject|PlainLocaleStringObject|PlainObject>}
  */
-export const i18n = async function i18n ({
+export const findLocaleStrings = async ({
   locales = navigator.languages,
   defaultLocales = ['en-US'],
-  defaults,
-  // eslint-disable-next-line max-len
-  // eslint-disable-next-line prefer-named-capture-group, unicorn/no-unsafe-regex
-  bracketRegex = /\{([^}]*?)(?:\|([^}]*))?\}/gu,
-  forceNodeReturn = false,
-  localesBasePath = '.',
-  localeResolver = DefaultLocaleResolver
-}) {
-  const strings = await promiseChainForValues(
+  localeResolver = DefaultLocaleResolver,
+  localesBasePath = '.'
+}) => {
+  // eslint-disable-next-line no-return-await
+  return await promiseChainForValues(
     [...locales, ...defaultLocales],
     async function getLocale (locale) {
       const url = localeResolver(locale, localesBasePath);
@@ -146,66 +142,195 @@ export const i18n = async function i18n ({
       }
     }
   );
-  return (key, substitutions, {dom} = {}) => {
-    let returnsDOM = false;
-    const str = (
-      key in strings && strings[key] && 'message' in strings[key] &&
-        // NECESSARY FOR SECURITY ON UNTRUSTED LOCALES
-        typeof strings[key].message === 'string'
-        ? strings[key].message
-        : typeof defaults === 'function'
-          ? defaults(key, strings)
-          : defaults === false
-            ? (() => {
-              throw new Error(`Key not found: (${key})`);
-            })()
-            : defaults[key].message
+};
 
-    );
-    if (!substitutions) {
-      return forceNodeReturn ? document.createTextNode(str) : str;
-    }
-    // Give chance to avoid this block when known to contain DOM
-    if (!dom) {
-      // Run this block to optimize non-DOM substitutions
-      const ret = str.replace(bracketRegex, (_, ky, arg) => {
-        let substitution = substitutions[ky];
-        if (typeof substitution === 'function') {
-          substitution = substitution(arg);
+/**
+ * @param {PlainObject} [cfg]
+ * @param {"rich"|"plain"|MessageStyleCallback} [cfg.messageStyle='rich']
+ * @returns {MessageStyleCallback}
+ */
+export const getMessageForKeyByStyle = ({
+  messageStyle = 'rich'
+} = {}) => {
+  return typeof messageStyle === 'function'
+    ? messageStyle
+    : (messageStyle === 'rich'
+      ? (obj, key) => {
+        if (key in obj && obj[key] && 'message' in obj[key] &&
+          // NECESSARY FOR SECURITY ON UNTRUSTED LOCALES
+          typeof obj[key].message === 'string'
+        ) {
+          return obj[key].message;
         }
-        returnsDOM = returnsDOM ||
-          (substitution && substitution.nodeType === 1);
-        return substitution;
-      });
-      if (!returnsDOM) {
-        return ret;
+        return false;
       }
-    }
-    const nodes = [];
-    let result;
-    let previousIndex = 0;
-    while ((result = bracketRegex.exec(str)) !== null) {
-      const {lastIndex} = bracketRegex;
-      const [bracketedKey, ky, arg] = result;
+      : (messageStyle === 'plain'
+        ? (obj, key) => {
+          if (key in obj && obj[key] && typeof obj[key] === 'string') {
+            return obj[key];
+          }
+          return false;
+        }
+        : (() => {
+          throw new TypeError(`Unknown \`messageStyle\` ${messageStyle}`);
+        })()
+      )
+    );
+};
+
+/* eslint-disable max-len */
+/**
+ * @param {PlainObject} cfg
+ * @param {string} [cfg.message]
+ * @param {false|LocaleStringObject|PlainLocaleStringObject|PlainObject} [cfg.defaults]
+ * @param {"rich"|"plain"|MessageStyleCallback} [cfg.messageStyle='rich']
+ * @param {MessageStyleCallback} [cfg.messageForKey] Defaults to getting `MessageStyleCallback` based on `messageStyle`
+ * @param {string} cfg.key Key to check against object of strings
+ * @returns {string}
+ */
+export const getStringFromMessageAndDefaults = ({
+  /* eslint-enable max-len */
+  message,
+  defaults,
+  messageStyle,
+  messageForKey = getMessageForKeyByStyle({messageStyle}),
+  key
+}) => {
+  // NECESSARY CHECK FOR SECURITY ON UNTRUSTED LOCALES
+  const str = typeof message === 'string'
+    ? message
+    : (defaults === false
+      ? (() => {
+        throw new Error(`Key value not found for key: (${key})`);
+      })()
+      : (defaults && typeof defaults === 'object'
+        ? messageForKey(defaults, key)
+        : (() => {
+          throw new TypeError(
+            `Default locale strings must resolve to \`false\` or an object!`
+          );
+        })()
+      )
+    );
+  if (str === false) {
+    throw new Error(`Key value not found for key: (${key})`);
+  }
+  return str;
+};
+
+/**
+ *
+ * @param {PlainObject} cfg
+ * @param {string} cfg.string
+ * @param {false|SubstitutionObject} [cfg.substitutions=false]
+ * @param {boolean} [cfg.dom=false]
+ * @param {RegExp} [cfg.bracketRegex=/\{([^}]*?)(?:\|([^}]*))?\}/gu]
+ * @param {boolean} [cfg.forceNodeReturn=false]
+ * @returns {string|DocumentFragment}
+ */
+export const getDOMForLocaleString = ({
+  string,
+  substitutions,
+  dom,
+  // eslint-disable-next-line max-len
+  // eslint-disable-next-line prefer-named-capture-group, unicorn/no-unsafe-regex
+  bracketRegex = /\{([^}]*?)(?:\|([^}]*))?\}/gu,
+  forceNodeReturn = false
+}) => {
+  if (!substitutions) {
+    return forceNodeReturn ? document.createTextNode(string) : string;
+  }
+  // Give chance to avoid this block when known to contain DOM
+  if (!dom) {
+    let returnsDOM = false;
+    // Run this block to optimize non-DOM substitutions
+    const ret = string.replace(bracketRegex, (_, ky, arg) => {
       let substitution = substitutions[ky];
       if (typeof substitution === 'function') {
         substitution = substitution(arg);
       }
-      const startBracketPos = lastIndex - bracketedKey.length;
-      if (startBracketPos > previousIndex) {
-        nodes.push(str.slice(previousIndex, startBracketPos));
-      }
-      nodes.push(substitution);
-      previousIndex = lastIndex;
+      returnsDOM = returnsDOM ||
+        (substitution && substitution.nodeType === 1);
+      return substitution;
+    });
+    if (!returnsDOM) {
+      return ret;
     }
-    if (previousIndex !== str.length) { // Get text at end
-      nodes.push(str.slice(previousIndex));
+  }
+  const nodes = [];
+  let result;
+  let previousIndex = 0;
+  while ((result = bracketRegex.exec(string)) !== null) {
+    const {lastIndex} = bracketRegex;
+    const [bracketedKey, ky, arg] = result;
+    let substitution = substitutions[ky];
+    if (typeof substitution === 'function') {
+      substitution = substitution(arg);
     }
+    const startBracketPos = lastIndex - bracketedKey.length;
+    if (startBracketPos > previousIndex) {
+      nodes.push(string.slice(previousIndex, startBracketPos));
+    }
+    nodes.push(substitution);
+    previousIndex = lastIndex;
+  }
+  if (previousIndex !== string.length) { // Get text at end
+    nodes.push(string.slice(previousIndex));
+  }
 
-    const container = document.createDocumentFragment();
+  const container = document.createDocumentFragment();
 
-    // console.log('nodes', nodes);
-    container.append(...nodes);
-    return container;
+  // console.log('nodes', nodes);
+  container.append(...nodes);
+  return container;
+};
+
+/* eslint-disable max-len */
+/**
+ * @param {PlainObject} [cfg]
+ * @param {string[]} [cfg.locales=navigator.languages] BCP-47 language strings
+ * @param {string[]} [cfg.defaultLocales=['en-US']]
+ * @param {string} [cfg.localesBasePath='.']
+ * @param {LocaleResolver} [cfg.localeResolver=DefaultLocaleResolver]
+ * @param {false|LocaleStringObject|PlainLocaleStringObject|PlainObject} [cfg.defaults]
+ * @param {"rich"|"plain"|MessageStyleCallback} [cfg.messageStyle='rich']
+ * @param {RegExp} [cfg.bracketRegex=/\{([^}]*?)(?:\|([^}]*))?\}/gu]
+ * @param {boolean} [cfg.forceNodeReturn=false]
+ * @returns {Promise<I18NCallback>} Rejects if no suitable locale is found.
+ */
+export const i18n = async function i18n ({
+  /* eslint-enable max-len */
+  locales,
+  defaultLocales,
+  localesBasePath,
+  localeResolver,
+  defaults,
+  messageStyle,
+  bracketRegex,
+  forceNodeReturn
+}) {
+  const strings = await findLocaleStrings({
+    locales, defaultLocales, localeResolver, localesBasePath
+  });
+  if (!strings || typeof strings !== 'object') {
+    throw new TypeError(`Locale strings must be an object!`);
+  }
+  const messageForKey = getMessageForKeyByStyle({messageStyle});
+  return (key, substitutions, {dom} = {}) => {
+    const message = messageForKey(strings, key);
+    const string = getStringFromMessageAndDefaults({
+      message,
+      defaults,
+      messageForKey,
+      key
+    });
+
+    return getDOMForLocaleString({
+      string,
+      substitutions,
+      forceNodeReturn,
+      dom,
+      bracketRegex
+    });
   };
 };
