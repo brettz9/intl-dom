baf8dde8c052e9560423ddfee6b013cc5d2d2053
Brett Zamir <brettz9@yahoo.com>
2019-10-21 11:35:20 +0800

    - Testing: `getMessageForKeyByStyle`

--- test/test.js
+++ test/test.js
@@ -90,3 +90,67 @@ describe('defaultLocaleResolver', function () {
     expect(path).to.equal(expected);
   });
 });
+
+describe('getMessageForKeyByStyle', function () {
+  describe('Default style', function () {
+    it('should process in rich style', function () {
+      const func = getMessageForKeyByStyle();
+      const localeObj = {
+        key: {
+          message: 'myKeyValue'
+        }
+      };
+      expect(func(localeObj, 'key')).to.equal('myKeyValue');
+      expect(func(localeObj, 'missingKey')).to.equal(false);
+    });
+  });
+  describe('Function style', function () {
+    it('should return function value', function () {
+      const localeObj = {
+        key: 'myKeyValue'
+      };
+      const func = getMessageForKeyByStyle({
+        messageStyle (obj, key) {
+          return obj[key] || false;
+        }
+      });
+      expect(func(localeObj, 'key')).to.equal('myKeyValue');
+      expect(func(localeObj, 'missingKey')).to.equal(false);
+    });
+  });
+  describe('Explicit rich style', function () {
+    it('should process in rich style', function () {
+      const func = getMessageForKeyByStyle({
+        messageStyle: 'rich'
+      });
+      const localeObj = {
+        key: {
+          message: 'myKeyValue'
+        }
+      };
+      expect(func(localeObj, 'key')).to.equal('myKeyValue');
+      expect(func(localeObj, 'missingKey')).to.equal(false);
+    });
+  });
+  describe('plain style', function () {
+    it('should process in plain style', function () {
+      const func = getMessageForKeyByStyle({
+        messageStyle: 'plain'
+      });
+      const localeObj = {
+        key: 'myKeyValue'
+      };
+      expect(func(localeObj, 'key')).to.equal('myKeyValue');
+      expect(func(localeObj, 'missingKey')).to.equal(false);
+    });
+  });
+  describe('Bad style', function () {
+    it('Should throw with an unknown style', function () {
+      expect(() => {
+        getMessageForKeyByStyle({
+          messageStyle: 'badStyle'
+        });
+      }).to.throw(TypeError, 'Unknown `messageStyle` badStyle');
+    });
+  });
+});
