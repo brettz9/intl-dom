a60d4e583611de60ce4ef510adc3fe1d6c828bed
Brett Zamir <brettz9@yahoo.com>
2019-10-21 10:43:08 +0800

    - Testing: Add all functions

--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
 import 'regenerator-runtime/runtime.js';
-import {DefaultLocaleResolver} from '../dist/index.esm.js';
+import {
+  promiseChainForValues,
+  DefaultLocaleResolver,
+  findLocaleStrings,
+  getMessageForKeyByStyle,
+  getStringFromMessageAndDefaults,
+  getDOMForLocaleString,
+  i18n
+} from '../dist/index.esm.js';
 
 describe('API', function () {
-  it('DefaultLocaleResolver', function () {
+  it('Should export functions', function () {
+    expect(promiseChainForValues).to.be.a('function');
     expect(DefaultLocaleResolver).to.be.a('function');
+    expect(findLocaleStrings).to.be.a('function');
+    expect(getMessageForKeyByStyle).to.be.a('function');
+    expect(getStringFromMessageAndDefaults).to.be.a('function');
+    expect(getDOMForLocaleString).to.be.a('function');
+    expect(i18n).to.be.a('function');
   });
 });
